e60a259d0e8be5b8fd9bc030d9820d3e7dd16326
alexpoe22 <alexpoe22@gmail.com>
2017-01-17 11:13:59 -0800

    css changes

--- client/js/pages/SpacedRep.js
+++ client/js/pages/SpacedRep.js
@@ -4,15 +4,18 @@ import QuestionCard from './pieces/questionscard';
 import InputCard from './pieces/input';
 import Counter from './pieces/counter';
 
-
-
-
 class SpacedRep extends React.Component {
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 	handleSubmit () {
+		//on submit, answer will be dispatched to server
+		//current text will 'unwrite itself using typewriter'
+		//then rewrite the answer in english
+		//if the answer it english matches the answer given
+		//do css highlight green, or animate 
+		//if not shake and highlight red. 
 		alert('hey')
 	}
 
@@ -22,7 +25,7 @@ class SpacedRep extends React.Component {
 				<h2> 
 				Spaced 
 				</h2>
-				<Link to="/"> Back </Link>
+				<Link to="/"><img className="home" src="./assets/home.png"/></Link>
 				<div className="container">	
 					<QuestionCard word={'Je suis'}/>
 					<InputCard funct={this.handleSubmit}/>
