f3c8d60389ef8a9db348a42981b83cd9a35e834b
alexpoe22 <alexpoe22@gmail.com>
2017-01-18 06:38:34 -0800

    merge

--- server/index.js
+++ server/index.js
@@ -6,19 +6,20 @@ import bodyParser from 'body-parser';
 import User from './models/user';
 mongoose.Promise = global.Promise;
 
-
 const algorithm = (questions, answer) => {
 
     if (answer === "true") {
-        let m = questions[0].ind * 2;
-        questions[0].ind += m;
+        questions[0].freq *= 2;
     } else {
-        questions[0].ind = 2;
-        questions[1].ind = 1;
+        questions[0].freq = 1;
     }
 
-    return questions.sort((a, b) => a.ind - b.ind);
+    let ind = questions[0].freq
+    let start = questions.slice(1, ind);
+    let end = questions.slice(ind);
+    let newQuestions = [...start, questions[0], ...end];
 
+    return newQuestions;
 }
 
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -49,16 +50,16 @@ app.get('/game', function(req, res) {
 app.post('/game', function(req, res) {
     User.create({
         score: 0,
-        questions: [{english: "one", french: "un", ind: 1}, 
-        {english: "two", french: "deux", ind: 2}, 
-        {english: "three", french: "trois", ind: 3}, 
-        {english: "four", french: "quatre", ind: 4}, 
-        {english: "five", french: "cinq", ind: 5}, 
-        {english: "six", french: "six", ind: 6}, 
-        {english: "seven", french: "sept", ind: 7}, 
-        {english: "eight", french: "huit", ind: 8}, 
-        {english: "nine", french: "neuf", ind: 9}, 
-        {english: "ten", french: "dix", ind: 10}, ]
+        questions: [{english: "one", french: "un", freq: 1}, 
+        {english: "two", french: "deux", freq: 1}, 
+        {english: "three", french: "trois", freq: 1}, 
+        {english: "four", french: "quatre", freq: 1}, 
+        {english: "five", french: "cinq", freq: 1}, 
+        {english: "six", french: "six", freq: 1}, 
+        {english: "seven", french: "sept", freq: 1}, 
+        {english: "eight", french: "huit", freq: 1}, 
+        {english: "nine", french: "neuf", freq: 1}, 
+        {english: "ten", french: "dix", freq: 1}]
     }, function(err, user) {
         if (err) {
             return res.status(500).json({
@@ -85,7 +86,7 @@ app.put('/game', function(req, res) {
             score += 1;
         }
         let questions = algorithm(current.questions, req.body.answer);
-
+        console.log(questions);
         User.findOneAndUpdate({__v: 0}, {$set:{score:score, questions:questions}},function(err, user){
             if (err) {
                 return res.status(500).json({
