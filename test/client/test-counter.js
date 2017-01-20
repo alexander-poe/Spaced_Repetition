5a4825c0ebaf664c2fc105dbc5ada81f41ea2eeb
Theresa Wilkin <theresawilkin@yahoo.com>
2017-01-20 12:11:58 -0500

    tests in progress

--- test/client/test-counter.js
+++ test/client/test-counter.js
@@ -7,7 +7,19 @@ const should = chai.should();
 
 describe('counter piece', () => {
 	it('should be a div element', () => {
-		
+		const count = 1;
+        const renderer = TestUtils.createRenderer();
+        renderer.render(<Counter count={count} />);
+        const result = renderer.getRenderOutput();
+        
+        result.type.should.equal('div');
+        result.props.className.should.equal('counter-box');
 
-	})
+        const child = result.props.children;
+        child.type.should.equal('span');
+        
+        const grandchild = child.props.children;
+        grandchild.type.should.equal('h3');
+        grandchild.props.children.should.equal(count);
+    });
 })
