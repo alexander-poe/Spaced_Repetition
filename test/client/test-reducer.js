49bf2195a3b037c812225d305cdd45ed439721d4
Theresa Wilkin <theresawilkin@yahoo.com>
2017-01-20 15:43:37 -0500

    tests

--- test/client/test-reducer.js
+++ test/client/test-reducer.js
@@ -1,17 +1,47 @@
 import reducer from '../../client/js/reducer'
+require('isomorphic-fetch')
+import chai from 'chai';
+const should = chai.should();
 
 describe('reducer', () => {
   it('should return the initial state', () => {
-    reducer(undefined, {}).should.equal(
-      {
-	question: {
-		english: "", 
-		french: "", 
-		freq: 1,
-	}, 
-	score: 2,
-	counter: 1,
-	language: 'french'
-	})
+    var initState = reducer(undefined, {});
+    initState.score.should.equal(2);
+    initState.counter.should.equal(1);
+    initState.language.should.equal('french');
+    initState.question.english.should.equal('');
+    initState.question.french.should.equal('');
+    initState.question.freq.should.equal(1);
+  })
+
+  it('should handle INCREMENT', () => {
+    var increment = reducer([], {
+      type: 'INCREMENT',
+      counter: 1
+    })
+    increment.counter.should.equal(2);
+  })
+  it('should handle DECREMENT', () => {
+    var decrement = reducer([], {
+      type: 'DECREMENT',
+      counter: 1
+    })
+    decrement.counter.should.equal(0);
+  })
+  it('should handle GET_CARD_SUCCESS', () => {
+    var question = {score: 0, question: {french: 'un', english: 'one'}}
+    var state = reducer([], {
+      type: 'GET_CARD_SUCCESS',
+      question: question
+    })
+    state.question.should.equal(question.question);
+  })
+  it('should handle ENGAGE_TYPEWRITER', () => {
+    var state = reducer([], {
+      type: 'ENGAGE_TYPEWRITER',
+      typewriter: 1
+    })
+    state.typewriter.should.equal(1);
   })
 });
+
